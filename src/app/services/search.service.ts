922ecb4885c35070a7ad59b41511d299c2a8d966
Joseph Subash Kanichai <40432493+t348575@users.noreply.github.com>
2020-09-08 12:26:07 +0530

    Update search.service.ts

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {AutoCompleteService} from 'ionic4-auto-complete';
+import {ProductModel} from '../models/product-model';
+import {Observable} from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
-export class SearchService {
-
+export class SearchService implements AutoCompleteService {
+    private results: ProductModel[] = [];
     constructor() { }
+    getResults(term: string): Observable<ProductModel[]> {
+        return new Observable<ProductModel[]>(observer => {
+        });
+    }
+    getItemLabel(item: any): any {
+    }
 }
